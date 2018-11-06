9c23d68076049a6a534807a3bf557249f66ebffb
Apollinaire <apollinaire.lecocq@live.fr>
2018-11-06 17:58:51 +0100

    comment all EN strings

--- en_US.js
+++ en_US.js
@@ -1,9 +1,9 @@
-import { addStrings } from 'meteor/vulcan:core';
-
-
-addStrings('en', {
-
-  "search.search": "Search",
-  "search.clear": "Clear search",
-
-});
+// import { addStrings } from 'meteor/vulcan:core';
+// 
+// 
+// addStrings('en', {
+// 
+//   "search.search": "Search",
+//   "search.clear": "Clear search",
+// 
+// });
