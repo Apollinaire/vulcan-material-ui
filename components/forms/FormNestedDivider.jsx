7d39672c123692866c172f84780e325f287ff6e0
Apollinaire <apollinaire.lecocq@live.fr>
2019-01-07 11:15:12 +0100

    restore forms nested divider

--- components/forms/FormNestedDivider.jsx
+++ components/forms/FormNestedDivider.jsx
@@ -3,22 +3,18 @@ import PropTypes from 'prop-types';
 import { replaceComponent } from 'meteor/vulcan:core';
 // import { FormattedMessage } from 'meteor/vulcan:i18n';
 import withStyles from '@material-ui/core/styles/withStyles';
-// import Divider from '@material-ui/core/Divider';
-
+import Divider from '@material-ui/core/Divider';
 
 const styles = theme => ({
-  
   divider: {
-    marginLeft: -24,
-    marginRight: -24,
+    // marginLeft: -24,
+    // marginRight: -24,
     marginTop: 16,
     marginBottom: 23,
   },
-  
 });
 
-
-const FormNestedDivider = ({ classes, label, addItem }) => <div className={classes.divider}/>;
+const FormNestedDivider = ({ classes, label, addItem }) => <Divider className={classes.divider} />;
 
 FormNestedDivider.propTypes = {
   classes: PropTypes.object.isRequired,
